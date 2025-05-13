5f58e87c17e0842df40addee2873cb83adda7967
Ben <eklundbenjaminj@gmail.com>
2025-05-13 16:49:54 -0700

    update with graduation info

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -27,9 +27,13 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
 
+<<<<<<< HEAD
+ALLOWED_HOSTS = ['beneklund.com', 'portfolio-site-production-41c1.up.railway.app', '*']
+=======
 ALLOWED_HOSTS = ['beneklund.com', 'www.beneklund.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://beneklund.com', 'https://www.beneklund.com']
+>>>>>>> fd607e787914304fe16f601535d8de44947be52d
 
 
 if 'CODESPACE_NAME' in os.environ:
